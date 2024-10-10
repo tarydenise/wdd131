22c3a7ddeb618f893a38337a93751886acf7709c
tarydenise <tarydenise@gmail.com>
2024-10-10 04:44:58 -0700

    W03 Assignment Place

---
+++ scripts/place.js
@@ -0,0 +1,13 @@
+const wcf = document.querySelector('#wcf');
+
+function windChill(temp, speed) {
+    return 13.12 + (0.6215 * temp) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * tempC * Math.pow(speed, 0.16));
+}
+
+let tempC = 10;
+let speed = 5;
+if (tempC <= 10 && speed > 4.8) {
+    wcf.textContent = windChill(tempC, speed).toFixed(1);
+} else {
+    wcf.textContent = "N/A";
+}
